5510f503693790c146957fde3bb3e83830843e10
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-01-16 14:52:42 -0300

    Criando profile de teste

---
+++ src/main/java/br/com/forclinic/config/service/DBService.java
@@ -0,0 +1,12 @@
+package br.com.forclinic.config.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+	
+	public void instanciarBancoDeDadosTest() {
+		// TODO implementar os dados iniciais para o profile test
+	}
+
+}
