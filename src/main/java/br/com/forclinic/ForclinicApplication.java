7a861aaab0130c9f572b3b92ad2fe9b8641cae4a
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2022-01-16 14:13:33 -0300

    Criação do projeto com Spring Boot

---
+++ src/main/java/br/com/forclinic/ForclinicApplication.java
@@ -0,0 +1,13 @@
+package br.com.forclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/forclinic/ForclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
